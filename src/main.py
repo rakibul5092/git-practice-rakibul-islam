7737301db1c1079b663d36089d9f8eca2bef6501
Rakibul Islam <rakibulislam@Rakibuls-MacBook-Pro.local>
2026-05-01 23:25:03 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from datetime import date
+from utils import add, subtract
 
 print("Name: Rakibul Islam")
 print("Today's date:", date.today())
+
+print("Addition:", add(10, 5))
+print("Subtraction:", subtract(10, 5))
