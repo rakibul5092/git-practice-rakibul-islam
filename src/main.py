faf4f00402a7b4834cc0e37c87e72705d75ee0f6
Rakibul Islam <rakibulislam@Rakibuls-MacBook-Pro.local>
2026-05-01 23:29:17 +0600

    Add multiply function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from datetime import date
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 print("Name: Rakibul Islam")
 print("Today's date:", date.today())
 
 print("Addition:", add(10, 5))
 print("Subtraction:", subtract(10, 5))
+print("Multiplication:", multiply(10, 5))
